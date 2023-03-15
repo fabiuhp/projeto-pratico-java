8524f1eb5eb9f124ce885f5245aa63cddbc034f2
fabio <fabiuhp@msn.com>
2023-03-15 14:13:14 -0300

    refat id de categoria

--- fc3-admin-do-catalogo/domain/src/main/java/com/fabiopereira/admin/catalogo/domain/category/Category.java
+++ fc3-admin-do-catalogo/domain/src/main/java/com/fabiopereira/admin/catalogo/domain/category/Category.java
@@ -1,11 +1,12 @@
 package com.fabiopereira.admin.catalogo.domain.category;
 
+import com.fabiopereira.admin.catalogo.domain.AggregateRoot;
+
 import java.time.Instant;
 import java.util.UUID;
 
-public class Category {
+public class Category extends AggregateRoot<CategoryID> {
 
-    private String id;
     private String name;
     private String description;
     private boolean active;
@@ -14,18 +15,18 @@ public class Category {
     private Instant deletedAt;
 
     private Category(
-            final String id,
-            final String name,
-            final String description,
-            final boolean active,
+            final CategoryID anId,
+            final String aName,
+            final String aDescription,
+            final boolean isActive,
             final Instant createdAt,
             final Instant updatedAt,
             final Instant deletedAt
     ) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.active = active;
+        super(anId);
+        this.name = aName;
+        this.description = aDescription;
+        this.active = isActive;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
         this.deletedAt = deletedAt;
@@ -36,12 +37,12 @@ public class Category {
             final String aDescription,
             final boolean isActive
     ) {
-        final String id = UUID.randomUUID().toString();
+        CategoryID id = CategoryID.unique();
         Instant now = Instant.now();
         return new Category(id, aName, aDescription, isActive, now, now, null);
     }
 
-    public String getId() {
+    public CategoryID getId() {
         return id;
     }
 
